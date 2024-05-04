5f6e1c244c21b966643f5ebf929bb2dbc909e921
ShivanshuPrajapati212 <152085574+ShivanshuPrajapati212@users.noreply.github.com>
2024-05-04 05:50:05 +0530

    Update main.py

--- Projects/main.py
+++ Projects/main.py
@@ -1,1 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+import json
 
+
+# Weather Status
+key = os.environ['KEY']
+
+query = input("Enter a search query: ")
+
+url = f"https://api.weatherapi.com/v1/current.json?key={os.environ['KEY']}&q={query}&aqi=no"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+data = json.loads(soup.text)
+
+print("Location :", data["location"]["name"])
+print("Time :", data["location"]["localtime"])
+print("Temperature in Celsius:", data["current"]["temp_c"])
+print("Temperature in Fahrenheit :", data["current"]["temp_f"])
+print("Wind Speed :", data["current"]["wind_kph"])
+print("Humidity :", data["current"]["humidity"])
